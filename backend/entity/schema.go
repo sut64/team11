e8a6545a5823ae550dfea676943d284b003f146b
pechkr2020 <pechjie3238@gmail.com>
2022-01-08 01:20:23 +0700

    close #67 -create ent PatientRight

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -3,5 +3,17 @@ package entity
 import (
 	//"time"
 
-	//"gorm.io/gorm"
+	"gorm.io/gorm"
 )
+
+
+type PatientRight struct {
+
+	gorm.Model
+
+	Name 		string
+
+	Discount	uint
+
+	//Bills		[]Bill		`gorm:"foreignKey:PatientRightID"`
+}
