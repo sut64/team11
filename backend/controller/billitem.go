7ed5ddb3faa343e53b9fe7a0b1ea05d857984439
pechkr2020 <pechjie3238@gmail.com>
2022-01-26 20:34:34 +0700

    close #86 - Create Billitem Controller

---
+++ backend/controller/billitem.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut64/team11/entity"
+)
+
+// GET /patientright/:id
+func GetBillItem(c *gin.Context) {
+	var billitem entity.BillItem
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM bill_items WHERE bill_id = ?", id).Scan(&billitem).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data":billitem})
+}
+
+// GET /patientrights
+func ListBillItem(c *gin.Context) {
+	var billitems []entity.BillItem
+	if err := entity.DB().Raw("SELECT * FROM bill_items").Scan(&billitems).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": billitems})
+}
